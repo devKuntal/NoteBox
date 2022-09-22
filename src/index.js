694b871d44780837d92558be4ab299370ab096cb
Kuntal <kmajhi.dev@gmail.com>
2022-09-22 13:48:20 +0530

    adding components to the app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+ReactDom.render(
+    <App />, 
+    document.querySelector("#root")
 );
