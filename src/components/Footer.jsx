c66aec78f9ef0a4a124921bc6a2677e926627645
Kuntal <kmajhi.dev@gmail.com>
2022-09-23 23:47:39 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
     return (
-        <p>Copyright</p>
+        <footer>made with <span>&#10084;&#65039;</span> by <a href="https://github.com/devkuntal" className="author">Kuntal</a></footer>
     );
 }
 
